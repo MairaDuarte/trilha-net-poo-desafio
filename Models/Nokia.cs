5471130062c2140261b75ad4cea9a881ea7abf7b
Maira <duarte.maira.marques@gmail.com>
2025-08-17 20:46:59 -0300

    Update Nokia.cs

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -2,7 +2,7 @@ using System.Runtime.InteropServices;
 
 namespace DesafioPOO.Models
 {
-    // implemn: Herdar da classe "Smartphone"
+    // Implementado: Herdar da classe "Smartphone"
     public class Nokia : Smartphone
     {
         public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
